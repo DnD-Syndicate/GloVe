1c7f26e514b9b84cfccaf1ae9582ad6ed9c71667
David Valpey <greengargoyle@hotmail.com>
2017-01-22 15:24:09 -0800

    fixed data import to only 2 columns: resource_id and parsed_text

--- src/spark.py
+++ src/spark.py
@@ -5,7 +5,7 @@ from src.prepare_court_data import import_dataframe
 opinion_df = import_dataframe(spark, 'opinion')
 
 # save just 1000 rows into a parquet file
-spark.createDataFrame(opinion_df.take(1000)).write.save('data/wash_state_1000_opinions.parquet', format='parquet', mode='overwrite')
+spark.createDataFrame(opinion_df.select('resource_id', 'parsed_text').take(1000)).write.save('data/wash_state_1000_opinions.parquet', format='parquet', mode='overwrite')
 
 # load parquet file into Spark
 df = spark.read.load('data/wash_state_1000_opinions.parquet')
