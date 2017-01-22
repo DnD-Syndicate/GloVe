d71b1aacae4a69636e7ea3822df0cfd345123e6f
David Valpey <greengargoyle@hotmail.com>
2017-01-22 11:50:06 -0800

    code to import and store and load a subset of the data in parquet format

---
+++ src/spark.py
@@ -0,0 +1,12 @@
+from src.prepare_court_data import import_dataframe
+
+
+# import all the opinion files from the archive
+opinion_df = import_dataframe(spark, 'opinion')
+
+# save just 1000 rows into a parquet file
+spark.createDataFrame(opinion_df.take(1000)).write.save('data/wash_state_1000_opinions.parquet', format='parquet', mode='overwrite')
+
+# load parquet file into Spark
+df = spark.read.load('data/wash_state_1000_opinions.parquet')
+
