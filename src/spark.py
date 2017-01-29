677ada29d01e03475f61076e8fca03e0ddd6b30e
David Valpey <greengargoyle@hotmail.com>
2017-01-29 14:30:41 -0800

    Needed to import string for str functions

--- src/spark.py
+++ src/spark.py
@@ -1,3 +1,4 @@
+import string
 from src.prepare_court_data import import_dataframe
 from pyspark.sql.functions import udf
 from pyspark.sql.types import ArrayType, StringType
@@ -8,7 +9,12 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 opinion_df = import_dataframe(spark, 'opinion')
 
 # save just 1000 rows into a parquet file
-spark.createDataFrame(opinion_df.select('resource_id', 'parsed_text').take(1000)).write.save('data/wash_state_1000_opinions.parquet', format='parquet', mode='overwrite')
+spark.createDataFrame(
+        opinion_df
+        .select('resource_id', 'parsed_text')
+        .take(1000)) \
+                .write
+                .save('data/wash_state_1000_opinions.parquet', format='parquet', mode='overwrite')
 
 # load parquet file into Spark
 df_opinions_unparsed = spark.read.load('data/wash_state_1000_opinions.parquet')
