dc9adf2a5db5f1d169091992d65d3982eadd0f9d
David Valpey <greengargoyle@hotmail.com>
2017-01-29 14:14:39 -0800

    Remove punctuation when tokenizing documents into lists of words

--- src/spark.py
+++ src/spark.py
@@ -16,13 +16,15 @@ df_opinions_unparsed = spark.read.load('data/wash_state_1000_opinions.parquet')
 # one time only download required for sent_tokenize
 nltk.download('punkt')
 
-# split each document into sentences
-sent_tokens = udf(lambda doc: sent_tokenize(doc.replace('\n', ' ').strip()), ArrayType(StringType()))
-token_lists = udf(lambda doc: [word_tokenize(sentence) for sentence in sent_tokenize(doc.replace('\n', ' ').strip())], ArrayType(ArrayType(StringType())))
-df_sentences = df_opinions_unparsed.withColumn('sents', sent_tokens('parsed_text'))
-
 # use a list generator in a spark UDF to first separate into sentences, and then word tokens
 # this is important because the GloVe implementation will find relationship ratios based on colocation within sentences.
-token_lists = udf(lambda doc: [word_tokenize(sentence) for sentence in sent_tokenize(doc.replace('\n', ' ').strip())], ArrayType(ArrayType(StringType())))
+token_lists = udf(lambda doc: [
+    word_tokenize(                                                                  # NLTK word tokenizer is smarter (can separate contractions)
+        sentence.translate(                                                         # translate can change one character into another
+            str.maketrans(string.punctuation, ' '*len(string.punctuation))          # make a translator that is smart enough to change punctuation within words
+            )
+        ) 
+    for sentence in sent_tokenize(doc.replace('\n', ' ').strip())],                 # bring the documents in divided into sentences
+    ArrayType(ArrayType(StringType())))                                             # declare nested array of strings for Spark
 df_words = df_opinions_unparsed.withColumn('sents', token_lists('parsed_text'))
 
