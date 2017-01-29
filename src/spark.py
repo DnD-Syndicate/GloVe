ef102718b1d936964257ad2c5dc5912010030ef9
David Valpey <greengargoyle@hotmail.com>
2017-01-29 12:10:15 -0800

    updated missing imports for nltk and spark

--- src/spark.py
+++ src/spark.py
@@ -1,4 +1,7 @@
 from src.prepare_court_data import import_dataframe
+from pyspark.sql.functions import udf
+from pyspark.sql.types import ArrayType, StringType
+from nltk.tokenize import sent_tokenize, word_tokenize
 
 
 # import all the opinion files from the archive
@@ -8,12 +11,12 @@ opinion_df = import_dataframe(spark, 'opinion')
 spark.createDataFrame(opinion_df.select('resource_id', 'parsed_text').take(1000)).write.save('data/wash_state_1000_opinions.parquet', format='parquet', mode='overwrite')
 
 # load parquet file into Spark
-df = spark.read.load('data/wash_state_1000_opinions.parquet')
+df_opinions_unparsed = spark.read.load('data/wash_state_1000_opinions.parquet')
 
 # one time only download required for sent_tokenize
 nltk.download('punkt')
 
 # split each document into sentences
-sent_tokens = udf(lambda doc: sent_tokenize(doc.replace('\n', ' ')), ArrayType(StringType()))
-df_sentences = df.withColumn('sents', sent_tokens('parsed_text'))
+sent_tokens = udf(lambda doc: sent_tokenize(doc.replace('\n', ' ').strip()), ArrayType(StringType()))
+df_sentences = df_opinions_unparsed.withColumn('sents', sent_tokens('parsed_text'))
 
