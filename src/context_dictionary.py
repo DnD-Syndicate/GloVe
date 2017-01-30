d0f2ea81fa3c17b1b8b58fd2759a9ab189312b6c
Dustin Mayeda <dmayeda2000@gmail.com>
2017-01-29 20:43:59 -0800

    changing the comment format.

--- src/context_dictionary.py
+++ src/context_dictionary.py
@@ -4,18 +4,22 @@ from itertools import chain
 
 def context(document, window_size=10):
     """
-    INPUT:
-    - document:  list, A list of lists where each list consists of the words which
-                        constitute a sentence.
-    - window_size:  int, The size of the symmetric context window to each side of
-                        the word.
+    INPUT
+    ------
+    document : list
+        A list of lists where each list consists of the words which constitute a
+        sentence.
+    window_size : int
+        The size of the symmetric context window to each side of the word.
 
 
-    OUTPUT:
-    - context_dictionary:  dict, A dictionary where the key is the context word and
-    the value is a Counter dictionary of the words which show up in the context window.
-    We will assume a default window size of 5 which will be truncated at the beginning
-    and end of the sentence.  For example, {'dog': {'cat': 2, 'mouse': 4}}.
+    OUTPUT
+    -------
+    context_dictionary : dict
+        A dictionary where the key is the context word and the value
+        is a Counter dictionary of the words which show up in the context window.
+        We will assume a default window size of 5 which will be truncated at the
+        beginning and end of the sentence.  For example, {'dog': {'cat': 2, 'mouse': 4}}.
     """
     context_dictionary = defaultdict(Counter)
     for sentence in document:
@@ -27,12 +31,15 @@ def context(document, window_size=10):
 
 def corpus_cooccurence(document_dicts):
     """
-    INPUT:
-    - document_dicts:   list, A list of document context dictionaries.
+    INPUT
+    ------
+    document_dicts : list
+        A list of document context dictionaries.
 
-    OUTPUT:
-    - corpus_dictionary:    dict, A dictionary encoding the cooccurence matrix of the
-                                    entire corpus.
+    OUTPUT
+    -------
+    corpus_dictionary : dict
+        A dictionary encoding the cooccurence matrix of the entire corpus.
     """
     corpus_dictionary = {}
     for dictionary in document_dicts:
