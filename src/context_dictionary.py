a2201f6fcf1f74d6a8cd0b86d0287a02f6aa6245
Dustin Mayeda <dmayeda2000@gmail.com>
2017-01-29 16:07:14 -0800

    added a function to merge the document cooccurence dictionaries to yield a corpus cooccurence dictionary

--- src/context_dictionary.py
+++ src/context_dictionary.py
@@ -15,7 +15,7 @@ def context(document, window_size=10):
     - context_dictionary:  dict, A dictionary where the key is the context word and
     the value is a Counter dictionary of the words which show up in the context window.
     We will assume a default window size of 5 which will be truncated at the beginning
-    and end of the sentence.  For example, {'dog': {'cat': 2, 'mouse': 4}}
+    and end of the sentence.  For example, {'dog': {'cat': 2, 'mouse': 4}}.
     """
     context_dictionary = defaultdict(Counter)
     for sentence in document:
@@ -24,3 +24,18 @@ def context(document, window_size=10):
             right_window = sentence[idx + 1: idx + window_size // 2]
             context_dictionary[word].update(chain(left_window, right_window))
     return context_dictionary
+
+def corpus_cooccurence(document_dicts):
+    """
+    INPUT:
+    - document_dicts:   list, A list of document context dictionaries.
+
+    OUTPUT:
+    - corpus_dictionary:    dict, A dictionary encoding the cooccurence matrix of the
+                                    entire corpus.
+    """
+    corpus_dictionary = {}
+    for dictionary in document_dicts:
+        for key in dictionary.keys():
+            corpus_dictionary[key].update(dictionary[key].elements())
+    return corpus_dictionary
