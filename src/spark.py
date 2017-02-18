ed3635654be2693aced4abbd92833b2211efe187
David Valpey <greengargoyle@hotmail.com>
2017-02-18 13:50:08 -0800

    more failed attempts to extract single tokens from the sentence lists

--- src/spark.py
+++ src/spark.py
@@ -27,13 +27,13 @@ import nltk; nltk.download('punkt')
 # use a list generator in a spark UDF to first separate into sentences, and then word tokens
 # this is important because the GloVe implementation will find relationship ratios based on colocation within sentences.
 token_lists = udf(lambda doc: [
-    word_tokenize(                                                                  # NLTK word tokenizer is smarter (can separate contractions)
-        sentence.translate(                                                         # translate can change one character into another
-            str.maketrans(string.punctuation, ' '*len(string.punctuation))          # make a translator that is smart enough to change punctuation within words
+    word_tokenize(                                              # NLTK word tokenizer is smarter (can separate contractions)
+        sentence.translate(                                     # translate can change one character into another
+            str.maketrans(string.punctuation, ' '*len(string.punctuation))  # a translator that changes punctuation within words
             )
         ) 
-    for sentence in sent_tokenize(doc.replace('\n', ' ').strip())],                 # bring the documents in divided into sentences
-    ArrayType(ArrayType(StringType())))                                             # declare nested array of strings for Spark
+    for sentence in sent_tokenize(doc.replace('\n', ' ').strip())],         # bring the documents in divided into sentences
+    ArrayType(ArrayType(StringType())))                                     # declare nested array of strings for Spark
 df_words = df_opinions_unparsed.withColumn('sents', token_lists('parsed_text'))
 
 # create a cooccurrence dictionary for each document
