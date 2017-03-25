c7ac24ab5643dfeaca3bc52366f137515cf37821
David Valpey <greengargoyle@hotmail.com>
2017-03-25 16:50:28 -0700

    Indentation error because of missing `\` in line 18

--- src/spark.py
+++ src/spark.py
@@ -15,7 +15,7 @@ spark.createDataFrame(
         opinion_df
         .select('resource_id', 'parsed_text')
         .take(1000)) \
-                .write
+                .write \
                 .save('data/wash_state_1000_opinions.parquet', format='parquet', mode='overwrite')
 
 # load parquet file into Spark
