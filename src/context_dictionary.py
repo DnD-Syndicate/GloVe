26f8e1286839a48f08af6c19e902705919061794
Dustin Mayeda <dmayeda2000@gmail.com>
2017-03-26 21:15:56 -0700

    rewrote code so that only standard dictionaries are used.

--- src/context_dictionary.py
+++ src/context_dictionary.py
@@ -1,5 +1,5 @@
-from collections import Counter
-from collections import defaultdict
+# from collections import Counter
+# from collections import defaultdict
 from itertools import chain
 
 def context(document, window_size=10):
@@ -21,12 +21,23 @@ def context(document, window_size=10):
         We will assume a default window size of 5 which will be truncated at the
         beginning and end of the sentence.  For example, {'dog': {'cat': 2, 'mouse': 4}}.
     """
-    context_dictionary = defaultdict(Counter)
+    # context_dictionary = defaultdict(Counter)
+    context_dictionary = {}
     for sentence in document:
         for idx, word in enumerate(sentence):
             left_window = sentence[max(0, idx - window_size // 2): idx]
             right_window = sentence[idx + 1: idx + window_size // 2]
-            context_dictionary[word].update(chain(left_window, right_window))
+            word_context = {}
+            for char in chain(left_window, right_window):
+                if word_context.get(char) == None:
+                    word_context[char] = 1
+                else:
+                    word_context[char] += 1
+            if context_dictionary.get(word) == None:
+                context_dictionary[word] = word_context
+            else:
+                context_dictionary[word].update(word_context)
+            # context_dictionary[word].update(chain(left_window, right_window))
     return context_dictionary
 
 def corpus_cooccurence(document_dicts):
